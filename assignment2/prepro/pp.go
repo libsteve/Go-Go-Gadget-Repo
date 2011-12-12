08fef76e0a44eec2d2c6d2027a195b892406ad6a
Steven Brunwasser <sbrunwasser@gmail.com>
2011-12-11 19:28:45 -0500

    Oops! prepro code was in pp.go. fixed.

--- assignment2/prepro/pp.go
+++ assignment2/prepro/pp.go
@@ -1,37 +1,11 @@
-/*
- * A group of functions related to preprocessing a text file
- *
- * Package Usage:
- *	<insert package usage here/>
- */
-package pp
+package main
 
 include (
 	"os"
 	"fmt"
+	"./prepro"
 )
 
-////////////////////////
-////////////////////////
-///
-/// WARNING!:	The following does not compile.
-///				This is just rough pseudo-pseudo-code
-///				to get our thoughts together.
-///
-////////////////////////
-////////////////////////
-
-func getCommands() map [string]func(/*place datastructure type here*/) {
-	return {
-		"define"	: func(/*place datastructure type here*/) {}
-		"if"		: ''
-		"ifdef"		: ''
-		"else"		: ''
-		"endif"		: ''
-		"include"	: ''
-	}
-}
-
-func getBuffer(/*input method*/) func() /*buffer return type?*/ {
+func main() {
 	
 }
---
+++ assignment2/prepro/prepro.go
@@ -0,0 +1,40 @@
+/*
+ * A group of functions related to preprocessing a text file
+ *
+ * Package Usage:
+ *	<insert package usage here/>
+ */
+package prepro
+
+include (
+	"os"
+	"fmt"
+)
+
+////////////////////////
+////////////////////////
+///
+/// WARNING!:	The following does not compile.
+///				This is just rough pseudo-pseudo-code
+///				to get our thoughts together.
+///
+////////////////////////
+////////////////////////
+
+func getCommands() map [string]func(/*place datastructure type here*/) {
+	return {
+		"define"	: func(/*place datastructure type here*/) {}
+		"if"		: ''
+		"ifdef"		: ''
+		"ifndef"	: ''
+		"else"		: ''
+		"endif"		: ''
+		"include"	: ''
+		"#"			: ''
+	}
+}
+
+func getBuffer(/*input method*/) func() /*buffer return type?*/ {
+	
+}
+
