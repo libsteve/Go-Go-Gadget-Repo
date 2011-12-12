cd694dfac157e14ba0b8f043abb93e8def6e106d
Kristen Mills <knm5153@g.rit.edu>
2011-12-11 21:59:40 -0500

    this could be right? maybe maybe?

--- assignment2/prepro/pp.go
+++ assignment2/prepro/pp.go
@@ -7,5 +7,17 @@ include (
 )
 
 func main() {
-	
+	if len( os.Args ) < 1 {
+		/* read from standard in */
+	}
+	else {
+		for _, arg := range args {
+			if arg == "-" {
+				/* read from standard in */
+			}
+			else {
+				/* read from this file*/
+			}
+		}	
+	}
 }
