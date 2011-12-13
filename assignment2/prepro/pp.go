43ce3159aba458751eb3dd673cf6f9e733494e2d
Kristen Mills <knm5153@g.rit.edu>
2011-12-13 15:46:02 -0500

    resources and web server related things

--- assignment2/prepro/pp.go
+++ assignment2/prepro/pp.go
@@ -1,28 +1,47 @@
 package main
 
-include (
+import (
 	"os"
 	"fmt"
 	"flag"
 	"bufio"
+	"http"
 	"./prepro"
 )
-
+func handler(w http.ResponseWriter, r *http.Request) {
+      file, _:= os.Open("index.html")
+	  in:= bufio.NewReader(file)
+	  for{
+		line,err := in.ReadString('\n')
+		if err != nil {
+			break
+		} else {
+			fmt.Fprint(w, line)
+		}
+	  }
+}
 func main() {
-	if len( os.Args ) < 1 {
+	var ws *bool
+	ws = flag.Bool("ws",false, "run as a webserver")
+	flag.Parse()
+	if *ws {
+		http.HandleFunc("/", handler)
+   		http.ListenAndServe(":6060", nil)
+		r, err := http.Get("http://localhost:6060/pp")
+	}else if flag.NArg() == 0{
 		in := bufio.NewReader(os.Stdin)
 		/* read from standard in */
-	}
-	else {
-		for _, arg := range args {
+
+	} else {
+		for _, arg := range flag.Args() {
 			if arg == "-" {
 				in := bufio.NewReader(os.Stdin)
 				/* read from standard in */
-			}
-			else {
-				in := os.Open(arg)
+			} else{
+				file,_ := os.Open(arg)
+				in := bufio.NewReader(file)
 				/* read from this file*/
-				in.Close()
+				file.Close()
 			}
 		}	
 	}
