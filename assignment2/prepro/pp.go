d8a0fb672693fc32e9fe57f97983686f74c3d324
Kristen Mills <knm5153@g.rit.edu>
2011-12-13 18:06:50 -0500

    I think the main is pretty much done except for throwing in your shit. oh wait i forgot to check for includes. i'll do it in in a second

--- assignment2/prepro/pp.go
+++ assignment2/prepro/pp.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"os"
+	"io/ioutil"
 	"fmt"
 	"flag"
 	"bufio"
 	"http"
-	"./prepro"
+//	"./prepro"
 )
 func handler(w http.ResponseWriter, r *http.Request) {
       file, _:= os.Open("index.html")
@@ -20,29 +21,47 @@ func handler(w http.ResponseWriter, r *http.Request) {
 		}
 	  }
 }
+
+func handlerInput(w http.ResponseWriter, r *http.Request){
+	//input := r.FormValue("input")
+	/*take the input and stick it in your thing*/
+}
+
 func main() {
 	var ws *bool
 	ws = flag.Bool("ws",false, "run as a webserver")
 	flag.Parse()
 	if *ws {
 		http.HandleFunc("/", handler)
-   		http.ListenAndServe(":6060", nil)
-		r, err := http.Get("http://localhost:6060/pp")
-	}else if flag.NArg() == 0{
-		in := bufio.NewReader(os.Stdin)
-		/* read from standard in */
-
-	} else {
-		for _, arg := range flag.Args() {
-			if arg == "-" {
-				in := bufio.NewReader(os.Stdin)
-				/* read from standard in */
+		http.HandleFunc("/pp", handlerInput)
+		http.ListenAndServe(":6060", nil)
+	} else if flag.Arg(0) == "-" || flag.NArg() == 0{
+		sin := bufio.NewReader(os.Stdin)
+		b :=  []byte{}
+		for {
+			l,_,_ := sin.ReadLine()
+			if string(l) == "."{
+				ioutil.WriteFile("temp.txt",b , 0600)
+				break
 			} else{
+				for _, a := range l{
+					b = append(b,a)
+				}
+				b = append(b,'\n')
+			}
+		}
+		file:= os.Open("temp.txt")
+		in := bufio.NewReader(file)
+		/* read from file in */
+		file.Close()
+		os.Remove("temp.txt")
+		for _, arg := range flag.Args() {
+			if arg != "-"{
 				file,_ := os.Open(arg)
 				in := bufio.NewReader(file)
 				/* read from this file*/
 				file.Close()
 			}
-		}	
+		}
 	}
 }
