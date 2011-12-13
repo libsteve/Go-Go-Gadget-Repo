7a9b00ecb88b6e3a08a1b34ca5d64a3182f43a32
Kristen Mills <knm5153@g.rit.edu>
2011-12-13 13:26:04 -0500

    Readers are there. have fun with it

--- assignment2/prepro/pp.go
+++ assignment2/prepro/pp.go
@@ -3,20 +3,26 @@ package main
 include (
 	"os"
 	"fmt"
+	"flag"
+	"bufio"
 	"./prepro"
 )
 
 func main() {
 	if len( os.Args ) < 1 {
+		in := bufio.NewReader(os.Stdin)
 		/* read from standard in */
 	}
 	else {
 		for _, arg := range args {
 			if arg == "-" {
+				in := bufio.NewReader(os.Stdin)
 				/* read from standard in */
 			}
 			else {
+				in := os.Open(arg)
 				/* read from this file*/
+				in.Close()
 			}
 		}	
 	}
