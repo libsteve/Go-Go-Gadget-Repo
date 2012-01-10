f44ced77ba6dbb8704ecc645ba5aeb0c0e3e0ade
Steven Brunwasser <sbrunwasser@gmail.com>
2012-01-10 00:24:42 -0500

    added resource.txt to assignment3

--- assignment3/ls/ls.go
+++ assignment3/ls/ls.go
@@ -6,7 +6,22 @@ import (
 
 func Ls(args []string) (string, os.Error) {
 	// args can be any of the following:
+	//		default
+	//			list one line at a time display files in alphabetical order
 	//		-n
+	//			display with information
 	//		-R
+	//			go through directories recursively
 	//		-t
+	//			sort by timestamp
 }
+
+// sort function to sort alphabetically
+// sort function to sort by timestamp
+
+// function to go through directories recursively
+// function not go recursively through directories
+
+// display function to only display the name
+// display function to display all information
+
