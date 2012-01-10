6b845f67e2c11e8744e9f16e630aa7cbe7e7f94e
Steven Brunwasser <sbrunwasser@gmail.com>
2012-01-10 00:01:06 -0500

    started work on assignment3 ls.go

---
+++ assignment3/ls/ls.go
@@ -0,0 +1,12 @@
+package ls
+
+import (
+	"os"
+)
+
+func Ls(args []string) (string, os.Error) {
+	// args can be any of the following:
+	//		-n
+	//		-R
+	//		-t
+}
---
+++ assignment3/ls/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"os"
+	"flag"
+	"fmt"
+	"ls"
+)
+
+func main() {
+	
+}
