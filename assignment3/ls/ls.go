6cbf56d8526497a2cab39a35dd4baf42445470cd
Steven Brunwasser <sbrunwasser@gmail.com>
2012-01-10 01:07:37 -0500

    filled in Ls func

--- assignment3/ls/ls.go
+++ assignment3/ls/ls.go
@@ -4,7 +4,7 @@ import (
 	"os"
 )
 
-func Ls(args []string) (string, os.Error) {
+func Ls(dirname string, args []string) (string, os.Error) {
 	// args can be any of the following:
 	//		default
 	//			list one line at a time display files in alphabetical order
@@ -14,14 +14,47 @@ func Ls(args []string) (string, os.Error) {
 	//			go through directories recursively
 	//		-t
 	//			sort by timestamp
+	lsdir := readdir
+	sort := alphasort
+	disp := namedisp
+	for index, arg := range args {
+		switch arg {
+		case "-n":
+			disp = infodisp
+		case "-R":
+			lsdir := recurdir
+		case "-t":
+			sort := timesort
+		default:
+			continue
+		}
+	}
+	result := ""
+	files := lsdir(dirname, sort, disp)
+	for index, file := range files {
+		result += file + "\n"
+	}
+	return result
+}
+
+// function to go through directories recursively
+func recurdir(filename string, sort func([]string) []string, disp func(string) string) []string {
+}
+// function not go recursively through directories
+func readdir(filename string, sort func([]string) []string, disp func(string) string) []string {
+}
+
+// display function to only display the name
+func namedisp(filename string) string {
+}
+// display function to display all information
+func infodisp(filename string) string {
 }
 
 // sort function to sort alphabetically
+func alphasort(files []string) []string {
+}
 // sort function to sort by timestamp
-
-// function to go through directories recursively
-// function not go recursively through directories
-
-// display function to only display the name
-// display function to display all information
+func timesort(files []string) []string {
+}
 
