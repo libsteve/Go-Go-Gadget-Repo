891a3d7993bd576fc8f4c7b677e246c84b8ee97f
Kristen Mills <knm5153@g.rit.edu>
2012-01-10 17:56:14 -0500

    MAIN MAIN MAIN

--- assignment3/ls/main.go
+++ assignment3/ls/main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
-	"os"
+	//"os"
 	"flag"
 	"fmt"
 	"ls"
 )
-
+/*
+ *
+ */
 func main() {
-	
+	var R *bool
+	var n *bool
+	var t *bool
+
+	R = flag.Bool("R", false, "go through directories recursively")
+	n = flag.Bool("n", false, "print with information")
+	t = flag.Bool("t", false, "sort files by modification time")
+	flag.Parse()
+
+	ls.Ls(flag.Arg(0), *n, *R, *t);
+
 }
