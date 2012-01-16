bffa8cd01efab65c47674b16bc94ac0edc46f1fa
Steven Brunwasser <sbrunwasser@gmail.com>
2012-01-15 21:13:28 -0500

    stubbed it out

--- assignment4/dag/edge_implementation.go
+++ assignment4/dag/edge_implementation.go
@@ -1,8 +1,26 @@
 package dag
 
 import(
+	"os"
 )
 
+/**
+ * Represents an edge on a DAG
+ */
 type Edge_struct struct{
+	targets []string
+}
 
+/**
+ * Perform an action.
+ */
+func(edge *Edge_struct) Action(target string, sources []string) os.Error {
+	return nil
+}
+
+/**
+ * Represent as a string.
+ */
+func(edge *Edge_struct) String() string {
+	return ""
 }
