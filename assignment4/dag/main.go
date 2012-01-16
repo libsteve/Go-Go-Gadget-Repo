1ce314ad65544efe9fe60ce6bb8dd710a58e860c
Steven Brunwasser <sbrunwasser@gmail.com>
2012-01-15 21:05:10 -0500

    main.go works :)

--- assignment4/dag/main.go
+++ assignment4/dag/main.go
@@ -3,31 +3,34 @@ package main
 import (
 	"flag"
 	"os"
+	"fmt"
 )
 
 func main() {
 	var force *bool
 
-	force = flag.Bool("-f", false, "use the given file as the source")
+	force = flag.Bool("f", false, "use the given file as the source")
 	flag.Parse()
 
-	args := flag.Args()
-
-	var file os.File
+	var file *os.File
+	var error os.Error
 	
 	/// open the file
-	if ok, arg1 := agrs[1]; ok == nil && force {
-		if file, error := os.Open(arg1); error != nil {
-			os.Stderr("404: file not found.")
+	if arg1 := flag.Arg(0); *force {
+		if file, error = os.Open(arg1); error != nil {
+			fmt.Fprintln(os.Stderr, "404: file not found.")
 			return
+		}
 	} else {
-		if file, error := os.Open("Makefile"); error != nil {
+		if file, error = os.Open("Makefile"); error != nil {
 			if file, error = os.Open("makefile"); error != nil {
-				os.Stderr("404:file not found")
+				fmt.Fprintln(os.Stderr, "404: file not found")
 				return
 			}
 		}
 	}
+
+	file.Close()
 
 	/// run the command with the file and the rest of the arguments
 
