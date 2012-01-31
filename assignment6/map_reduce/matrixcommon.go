6849803a9511ad398d97e6ee41d4608161ad79bb
Kristen Mills <knm5153@g.rit.edu>
2012-01-30 23:42:37 -0500

    compiles. No mains yet so it probably just doesn't work

--- assignment6/map_reduce/matrixcommon.go
+++ assignment6/map_reduce/matrixcommon.go
@@ -1,4 +1,4 @@
-package matrixcommon
+package matrix
 
 type Matrix_struct  struct {
 	Name string
@@ -6,14 +6,39 @@ type Matrix_struct  struct {
 	Matrix [][]float64
 }
 
-func New(name string, rows, cols int) *Matrix_struct{
+func NewMatrix(name string, rows, cols int) *Matrix_struct{
 	news := new(Matrix_struct)
 	news.Name = name
 	news.Rows = rows
 	news.Cols = cols
 	news.Matrix = make([][]float64, rows)
 	for i, _ := range news.Matrix {
-		m[i] = make([]int, cols)
+		news.Matrix[i] = make([]float64, cols)
 	}
+	return news
+}
+
+func NewMake(name string, rows, cols int) *Make{
+	news := new(Make)
+	news.Name = name
+	news.Rows = rows
+	news.Cols = cols
+	return news
+}
+
+func NewGet(name string, i, j int) *Get{
+	news := new(Get)
+	news.Name = name
+	news.I = i
+	news.J = j
+	return news
+}
+
+func NewSet(name string, i, j int, value float64) *Set{
+	news := new(Set)
+	news.Name = name
+	news.I = i
+	news.J = j
+	news.Value = value
 	return news
 }
