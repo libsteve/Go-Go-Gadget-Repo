daa60c761faa3391fb5ee196ad481c2bc8c6fc99
Steven Brunwasser <sbrunwasser@gmail.com>
2012-02-01 17:08:19 -0500

    made documentation for the registry programs

--- assignment6/map_reduce/doc-registry-client.go
+++ assignment6/map_reduce/doc-registry-client.go
@@ -1,1 +1,14 @@
+/*
+Runs the client for the registry.
+
+Takes input from standard-in and executes the following commands:
+
+	Command:			Description:
+
+	help				display a list of commands
+	bind: KEY, VALUE 	bind the KEY to the VALUE
+	lookup: KEY			find the VALUE for th KEY
+	remove: KEY			remove the KEY and the VALUE
+	close 				close the scession
+*/
 package documentation
--- assignment6/map_reduce/doc-registry-server.go
+++ assignment6/map_reduce/doc-registry-server.go
@@ -1,1 +1,48 @@
+/*
+Runs the registry server.
+Is the registry server.
+*/
 package documentation
+
+/*
+Create a new registry
+*/
+func NewReg() *Reg {}
+
+/*
+Bind a key to a value
+
+Parameters:
+	args - a bind struct with a key and data
+	handle - a pointer to an int. this will be the return 
+			 value for the handle that the key is assigned to
+
+Returns:
+	nil
+*/
+func (reg *Reg) Bind(args registry.Bind, handle *int) os.Error {}
+
+/*
+Find the value for a key
+
+Parameters:
+	key - the key string to look for
+	pointer - a pointer to anything. this will be the returned data for the key
+
+Returns:
+	os.Error if the key is not found, nil otherwise
+*/
+func (reg *Reg) Lookup(key string, pointer *interface{}) os.Error {}
+
+/*
+Remove a key and value
+
+Parameters:
+	args - a remove struct with a key and number to remove
+	number - a pointer to an int. this will be the return 
+			 the amount of entries removed
+
+Returns:
+	os.Error if the key is not found, nil otherwise
+*/
+func (reg *Reg) Remove(args registry.Remove, number *int) os.Error {}
