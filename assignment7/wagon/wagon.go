23493b14338d2c8d2922a7245647e9491de8733b
Steven Brunwasser <sbrunwasser@gmail.com>
2012-02-06 18:46:08 -0500

    work on wagon wheels

--- assignment7/wagon/wagon.go
+++ assignment7/wagon/wagon.go
@@ -4,12 +4,33 @@ type Wheel struct {
 	Value string
 	X int
 	Y int
-	Next Wheel
-	Prev Wheel
+	Next *Wheel
+	Prev *Wheel
 }
 
 type Wagon struct {
-	Head Wheel
-	Tail Wheel
+	Head *Wheel
+	Tail *Wheel
 }
 
+func NewWheel(value string) *Wheel {
+	w = new(Wheel)
+	w.Value = value
+	return w
+}
+
+func NewWagon() *Wagon {
+	w = new(Wagon)
+	return w
+}
+
+func (w *Wagon) Add(wheel *Wheel) {
+	if w.Tail == nil {
+		w.Head = wheel
+		w.Tail = wheel
+	} else {
+		w.Tail.Next = wheel
+		wheel.Prev = w.Tail
+		w.Tail = wheel
+	}
+}
