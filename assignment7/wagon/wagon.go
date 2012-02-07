0a1abdceda19df94585c362a81282a33b9bbe538
Steven Brunwasser <sbrunwasser@gmail.com>
2012-02-06 19:01:36 -0500

    added comments to wagon.go

--- assignment7/wagon/wagon.go
+++ assignment7/wagon/wagon.go
@@ -1,5 +1,18 @@
+/*
+A package to represent a collection of wheels as a wagon.
+*/
 package wagon
 
+/*
+The Wheel Struct.
+
+Has:
+	Value - represent the wheel
+	X - the x position
+	Y - the y position
+	Next - the next wheel in the chain
+	Prev - the previous wheel in the chain
+*/
 type Wheel struct {
 	Value string
 	X int
@@ -8,22 +21,72 @@ type Wheel struct {
 	Prev *Wheel
 }
 
+/*
+The Wagon Struct.
+
+HasL
+	Head - the beginning of the wagon train
+	Tail - the end of the wagon train
+*/
 type Wagon struct {
 	Head *Wheel
 	Tail *Wheel
 }
 
+/*
+Create a new Wheel.
+
+Parameters:
+	value - the value of the wheel
+	x - the x position
+	y - the y position
+
+Returns:
+	*Wheel - a pointer to a wheel with the given values
+*/
+func NewWheel(value string, x int, y int) *Wheel {
+	w = new(Wheel)
+	w.Value = value
+	w.X = x
+	w.Y = y
+	return w
+}
+
+/*
+Create a new Wheel.
+
+Parameters:
+	value - the value of the wheel
+
+Returns:
+	*Wheel - a pointer to a wheel with the given value
+*/
 func NewWheel(value string) *Wheel {
 	w = new(Wheel)
 	w.Value = value
 	return w
 }
 
+/*
+Create a new Wagon train.
+
+Returns:
+	*Wagon - a pointer to a fresh new wagon train.
+*/
 func NewWagon() *Wagon {
 	w = new(Wagon)
 	return w
 }
 
+/*
+Add a wheel to the wagon train.
+
+Method for:
+	*Wagon - a pointer to the wagon to add a wheel to
+
+Parameters:
+	wheel - a pointer to the wheel to add to the wagon
+*/
 func (w *Wagon) Add(wheel *Wheel) {
 	if w.Tail == nil {
 		w.Head = wheel
