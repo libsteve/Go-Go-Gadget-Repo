6a2665c25bce2de691fc73b6581b46a615c197eb
Steven Brunwasser <sbrunwasser@gmail.com>
2012-02-08 17:43:01 -0500

    we have doc

---
+++ assignment7/wagon/doc.go
@@ -0,0 +1,7 @@
+/*
+This program runs a wagon train where wheels can be added to the head or the tail.
+
+Usage:
+	./wagon - run the program through the script to set the terminal to raw
+*/
+package documentation
