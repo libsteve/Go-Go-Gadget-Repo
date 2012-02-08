d3506fced48017ead17a59cbbab0bac28f20db11
Steven Brunwasser <sbrunwasser@gmail.com>
2012-02-08 01:57:56 -0500

    updated screen.go

--- assignment7/wagon/screen.go
+++ assignment7/wagon/screen.go
@@ -3,11 +3,17 @@ A package to represent a grid-based screen of certain integer height and width.
 */
 package screen
 
-import "fmt"
+import ( "os"; "strconv"; "strings"; "exec" )
 //import "terminal"
 
 /*
 A Screen Struct to represent a screen with height and width.
+
+Variables:
+	Height - the number of rows of the screen
+	Width - the number of columns of the screen
+	Buffer - the cahracter buffer to print to the screen
+	DefaultChar - the default character to draw to the screen if the buffer at that position is empty
 */
 type Screen struct {
 	Height int
@@ -30,8 +36,7 @@ func NewScreen(height int, width int) *Screen {
 	s := new(Screen)
 	s.Height = height
 	s.Width = width
-	s.Buffer = make([][]string, height)
-	for i, _ := range s.Buffer { s.Buffer[i] = make([]string, width) }
+	s.EmptyBuffer()
 	s.DefaultChar = " "
 	return s
 }
@@ -88,15 +93,14 @@ Post:
 	s.Buffer - the screen's buffer is reset
 */
 func (s *Screen) Print() {
-	for _, line_arr := range s.Buffer {
-		var line string
-		for _, char := range line_arr {
-			if char == "" { line += s.DefaultChar } else { line += char }
+	for r, line_arr := range s.Buffer {
+		for c, char := range line_arr {
+			if char == "" { char = s.DefaultChar }
+			os.Stdout.Write(([]byte)("\033["+strconv.Itoa(r)+";"+strconv.Itoa(c)+"H"+char))
+			os.Stdout.Sync()		
 		}
-		fmt.Println(line)
 	}
-	s.Buffer = make([][]string, s.Height)
-	for i, _ := range s.Buffer { s.Buffer[i] = make([]string, s.Width) }
+	s.EmptyBuffer()
 }
 
 /*
@@ -115,6 +119,53 @@ Post:
 func (s *Screen) ChangeScreenSize(height int, width int) {
 	s.Height = height
 	s.Width = width
-	s.Buffer = make([][]string, height)
-	for i, _ := range s.Buffer { s.Buffer[i] = make([]string, width) }
+	s.EmptyBuffer()
+}
+
+func (s *Screen) EmptyBuffer() {
+	s.Buffer = make([][]string, s.Height)
+	for i, _ := range s.Buffer { s.Buffer[i] = make([]string, s.Width) }
+}
+
+/*
+Get the dimensions of the standard out screen.
+
+Returns:
+	int - the number of rows on the screen
+	int - the number of columns on the screen
+	os.Error - the error if the function failed, nil if successful
+*/
+func GetScreenDimensions() (int, int, os.Error) {
+	c := exec.Command("stty", "size")
+	raw_out, err := c.Output()
+	if err == nil {
+		out := (string)(raw_out)
+		nums := strings.Split(out, " ")
+		row, err1 := strconv.Atoi(nums[0])
+		col, err2 := strconv.Atoi(nums[1])
+		if err1 != nil || err2 != nil { return row, col, nil }
+		return 0, 0, os.NewError("Convertion Error")
+	}
+	return 0, 0, err
+}
+
+/*
+Prepare the standard in and out screens for raw mode.
+
+Returns:
+	func() os.Error - a function used for restoring the screens to their pre-raw mode
+	os.Error - the error if the function failed, nil if successful
+*/
+func PrepScreenForRaw() (func() os.Error, os.Error) {
+	c := exec.Command("stty", "-g")
+	raw_out, err := c.Output()
+	if err == nil {
+		out := (string)(raw_out)
+		reset_func := func() os.Error {
+			c = exec.Command("stty", out)
+			return c.Run()
+		}
+		return reset_func, nil
+	}
+	return func() os.Error { return err }, err
 }
